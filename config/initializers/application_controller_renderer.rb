e475b55b52547f5d2d99838233458868156bff3e
HonokaHarakawa <wow.hono313@gmail.com>
2022-02-17 13:41:53 +0000

    [finish]meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '8c2c224888d26cf3d674fb94ce0f649b5ee721bad7c1595294a7054a226120da6b0e880944b952e1384737773d7d95789dafbac8d2bc1bfc3f0f7e039131b5e9'
